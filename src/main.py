5696951835674bced65d77b162f826931c19fb1a
Yahya9102 <yaya_hussein@hotmail.com>
2025-11-19 18:51:33 +0100

    Implement user management console with add, list, search, and delete functionalities

---
+++ src/main.py
@@ -0,0 +1,71 @@
+from users import add_user, list_users, user_exists, delete_user
+
+
+def show_menu():
+    print("1. Lägg till användare")
+    print("2. Lista alla användare")
+    print("3. Kontrollera om användaren finns")
+    print("4. Ta bort användare")
+    print("5. Avsluta")
+
+
+
+def handle_search_user():
+    name = input("Ange ett användarnamn: ")
+    exists, message = user_exists(name)
+    print(exists,message)
+
+
+
+
+def handle_add_user():
+    name = input("Ange ett användarnamn: ")
+    sucess = add_user(name)
+
+    if sucess:
+        print(f"Användaren'{name.strip()}' lades till" )
+    else:
+        print("Kunde inte lägga till användare")
+        print(" Namnet kanske är tom eller redan finns")    
+
+
+def handle_list_users():
+    all_users = list_users()
+
+    if len(all_users) == 0:
+        print("Det finns inga användare i listan..")
+    else:
+        for index, name in enumerate(all_users, start=1):
+            print(f"{index}, {name}")    
+
+
+def handle_delete_user():
+    name = input("Ange ett användarnamna att ta bort")
+    sucess, message = delete_user(name)
+    print(sucess, message)
+
+
+def main():
+    while True:
+        show_menu()
+
+        choice = input("Välj ett alternativ: ").strip()
+        
+        if choice == "1":
+            handle_add_user()
+        elif choice == "2":
+            handle_list_users()
+        elif choice == "3":
+            handle_search_user()
+        elif choice == "4": 
+            handle_delete_user()
+        elif choice == "5":
+            print("Avslutar programmet.")
+            break
+        else:
+            print("Ogiltigt val. Försök igen.")
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/users.py
@@ -0,0 +1,48 @@
+# Tom lista som vi kan använda för att spara våra usernames i 
+users = []
+
+
+def add_user(name):
+
+    # Rensa upp användarnamnet genom att ta bort eventuella mellanslag
+    cleaned_name = name.strip()
+
+    if len(cleaned_name) == 0:
+        return False
+
+    # Kontrollera om användarnamnet redan finns i listan 
+    if cleaned_name in users:
+        return False
+
+    users.append(cleaned_name)
+    return True
+
+
+
+def list_users():
+    return users
+
+
+
+def user_exists(name):
+
+    cleaned_name = name.strip()
+    if cleaned_name in users:
+        return True, f"Användare '{cleaned_name}' finns i listan."
+    else:
+        return False, f"Användare ''{cleaned_name} finns inte i listan."
+    
+
+
+
+def delete_user(name):
+
+    cleaned_name = name.strip()
+
+
+    if cleaned_name in users:
+        users.remove(cleaned_name)
+        return True, f"Användare '{cleaned_name}' har tagits bort."
+    else:
+        return False, f"Användare '{cleaned_name}' finns inte i listan."
+
