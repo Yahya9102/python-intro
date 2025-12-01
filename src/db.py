a17ecd09d307a1598c7621aef610514adceae159
Yahya9102 <yaya_hussein@hotmail.com>
2025-12-01 17:29:16 +0100

    Update database connection handling and add .env support

--- src/db.py
+++ src/db.py
@@ -1,7 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "mysql+pymysql://root:12345@localhost:3306/users"
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+
+
+# DATABASE_URL = "mysql+pymysql://root:12345@localhost:3306/users"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
